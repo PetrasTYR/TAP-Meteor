154fe17819b8f11d136217cd91f1a0d8b38c908f
Petras Tan <petrastyr@gmail.com>
2022-09-09 21:48:02 +0800

    feat: implemented govtAPI to call traffic image endpoint and weather endpoint feat: http helper methods for easier calling of http requests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import { useQuery } from '@tanstack/react-query'
+import govtAPI from './api/govtAPI';
 
 function App() {
+  // * test react-query
+  const { data: trafficData, error: getTrafficDataError } = useQuery(
+    ['trafficData', '2021-01-01T00:00:00'],
+    async () => {
+      const { data } = await govtAPI.getTrafficImage('2021-01-01T00:00:00');
+      return data;
+    }
+  )
+
+  if (getTrafficDataError) {
+    console.log('error');
+    console.log(getTrafficDataError);
+  }
+
+  useEffect(() => {
+    if (trafficData) {
+      console.log(trafficData);
+    }
+  }, [trafficData])
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const DATA_GOV_BASE_URL = 'https://api.data.gov.sg/';
+export const VERSION_ONE_API_PREFIX = '/v1';
